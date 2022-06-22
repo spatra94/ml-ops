c616b076d1ae736983b742760d52620206608fd4
unknown <spatra@veracitiz.com>
2022-06-22 18:15:30 +0530

    latest

---
+++ setup.py
@@ -0,0 +1,11 @@
+from re import M
+from setuptools import setup,find_packages
+
+setup(
+    name = "src",
+    version="0.0.1",
+    description="MLOps Pipeline for WinqQ",
+    author="Ashish",
+    packages=find_packages(),
+    license="MIT"
+)
